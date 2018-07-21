ebe41785db68c11abfc3ec506a96eaf7073f1943
Burak Canik <canik.burak@gmail.com>
2018-07-21 17:19:48 +0300

    Basic animation implemented.
    It's based on different "poses" of a voxel model, created in MagicaVoxel. "Mesh" of the gameobject is switched on runtime to make it "animate". Not ideal, but good enough for prototyping purposes.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,25 +13,54 @@ public class Enemy : MonoBehaviour, IObservable
 	[ Header( "Unity Setup" ) ]
 	public static	GameObject			m_Goal;
 	public			NavMeshAgent		m_Agent;
+	public          Mesh[]              m_Meshes; // 0: Idle, 1: Run01, 2: Run02.
+	public          Material[]          m_Materials; // 0: Idle, 1: Run01, 2: Run02.
+	public			float				m_AnimFrequency = 0.2f;
 
 	// Attributes.
 	private         float               m_Health;
 
 	// Other.
+	private         MeshFilter			m_MeshFilter;
+	private         MeshRenderer		m_MeshRenderer;
+	private         float               m_AnimTimer;
+	private         int                 m_AnimIdx = 0; // 0: Idle, 1: Run01, 2: Run02.
 	private         List< IObserver >	m_Observers;
 
 	//// Unity Callbacks ////
 	private void Start()
 	{
-		m_Health      = m_StartHealth;
+		m_Health       = m_StartHealth;
 
-		m_Goal        = GameObject.FindGameObjectWithTag( "Goal" );
-		m_Agent.speed = m_MoveSpeed;
-		m_Observers   = new List< IObserver >();
+		m_AnimTimer	   = m_AnimFrequency;
+
+		m_Goal         = GameObject.FindGameObjectWithTag( "Goal" );
+		m_Agent.speed  = m_MoveSpeed;
+		m_MeshFilter   = GetComponent< MeshFilter >();
+		m_MeshRenderer = GetComponent< MeshRenderer >();
+		m_Observers    = new List< IObserver >();
 	}
 	private void Update()
 	{
 		m_Agent.SetDestination( m_Goal.transform.position );
+		if( m_Agent.velocity.sqrMagnitude > 0.005f )
+		{
+			if( m_AnimTimer <= 0.0f )
+			{
+				m_AnimIdx = ( m_AnimIdx + 1 ) % 3;
+				UpdateAnimation();
+				m_AnimTimer = m_AnimFrequency;
+			}
+			else
+			{
+				m_AnimTimer -= Time.deltaTime;
+			}
+		}
+		else if( m_AnimIdx != 0 )
+		{
+			m_AnimIdx = 0;
+			UpdateAnimation();
+		}
 	}
 	private void OnEnable()
 	{
@@ -75,6 +104,11 @@ public class Enemy : MonoBehaviour, IObservable
 		PoolsManager.Despawn( gameObject );
 		WaveSpawner.INSTANCE.OnEnemyDied();
 		Notify();
+	}
+	private void UpdateAnimation()
+	{
+		m_MeshFilter.mesh       = m_Meshes[ m_AnimIdx ];
+		m_MeshRenderer.material = m_Materials[ m_AnimIdx ];
 	}
 	private void Notify()
 	{
