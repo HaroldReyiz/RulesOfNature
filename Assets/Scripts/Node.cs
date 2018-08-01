64857f9a9acb5a7db5e68a505637f6d56fc92c8d
Burak Canik <canik.burak@gmail.com>
2018-08-02 00:10:24 +0300

    Due to a bug, multiple towers could be built on a single node. Fixed.

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 public class Node : MonoBehaviour
 {
 	//// Fields ////
+	public  bool    m_IsOccupiedByTower = false;
 
 	//// Unity Callbacks ////
 	private void OnMouseDown()
 	{
-		if( EventSystem.current.IsPointerOverGameObject() ) // Check if we clicked on an UI object.
+		if( m_IsOccupiedByTower || EventSystem.current.IsPointerOverGameObject() ) // Check if we clicked on an UI object.
 		{
 			return;
 		}
@@ -16,18 +17,25 @@ public class Node : MonoBehaviour
 		if( BuildManager.INSTANCE.IsInBuildMode /*&& player has enough money */ )
 		{
 			BuildManager.INSTANCE.BuildTower( transform.position );
+			m_IsOccupiedByTower = true;
 		}
 	}
 	private void OnMouseOver()
 	{
+		if( m_IsOccupiedByTower )
+		{
+			return;
+		}
+
 		if( Input.GetMouseButton( 0 ) && BuildManager.INSTANCE.IsInBuildMode /*&& player has enough money */ )
 		{
 			BuildManager.INSTANCE.BuildTower( transform.position );
+			m_IsOccupiedByTower = true;
 		}
 	}
 	private void OnMouseEnter()
 	{
-		if( EventSystem.current.IsPointerOverGameObject() ) // Check if we clicked on an UI object.
+		if( m_IsOccupiedByTower || EventSystem.current.IsPointerOverGameObject() ) // Check if we clicked on an UI object.
 		{
 			return;
 		}
